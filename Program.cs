e37db7154160a9c6a7848376e5cc737808ff0a84
Rodrigo <rodrigo.empoleon@gmail.com>
2021-10-01 22:08:23 -0600

    Remove // validation as it's not needed, change order of program execution

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ namespace Lab_1___1156813
         static void Main(string[] args)
         {
             string regexp = Console.ReadLine();
-            object value = new DataTable().Compute(regexp, null);
-            Console.WriteLine(value.ToString());
+            //Parse the expression
             Parser parser = new Parser();
             parser.Parse(regexp);
             Console.WriteLine("Expresion OK");
+            //If it's valid then solve the arithmetic expression by using DataTable Compute
+            object value = new DataTable().Compute(regexp, null);
+            Console.WriteLine(value.ToString());
             
             Console.ReadLine();
         }
--- Scanner.cs
+++ Scanner.cs
@@ -6,12 +6,11 @@ namespace Lab_1___1156813
     {
         private string _regexp = "";
         private int _index = 0;
-        private int _state = 0;
+
         public Scanner(string regexp)
         {
             _regexp = regexp + (char)TokenType.eof;
             _index = 0;
-            _state = 0;
         }
         public Token GetToken()
         {
@@ -20,73 +19,32 @@ namespace Lab_1___1156813
             while (!tokenFound)
             {
                 char peek = _regexp[_index];
-                switch (_state)
+                //Whitespace removal
+                while (char.IsWhiteSpace(peek))
                 {
-                    case 0:
-
-                        //Whitespace removal
-                        while (char.IsWhiteSpace(peek))
-                        {
-                            _index++;
-                            peek = _regexp[_index];
-                        }
-                        switch (peek)
-                        {
-                            case '\\':
-                                _state = 1;
-                                break;
-                            case (char)TokenType.lParen:
-                            case (char)TokenType.rParen:
-                            case (char)TokenType.plus:
-                            case (char)TokenType.minus:
-                            case (char)TokenType.mult:
-                            case (char)TokenType.div:
-                            case (char)TokenType.eof:
-                                tokenFound = true;
-                                result.tag = (TokenType)peek;
-                                break;
-                            default:
-                                tokenFound = true;
-                                result.tag = TokenType.number;
-                                result.value = peek;
-                                break;
-                        }
-                        break;
-                    case 1:
-                        switch (peek)
-                        {
-                            case (char)TokenType.lParen:
-                            case (char)TokenType.rParen:
-                            case (char)TokenType.plus:
-                            case (char)TokenType.minus:
-                            case (char)TokenType.mult:
-                            case (char)TokenType.div:
-                            case (char)TokenType.eof:
-                            case '\\':
-                            case ' ':
-                                tokenFound = true;
-                                result.tag = TokenType.number;
-                                result.value = peek;
-                                break;
-                            case 'E':
-                                tokenFound = true;
-                                result.tag = TokenType._null;
-                                break;
-                            case '0':
-                                tokenFound = true;
-                                result.tag = TokenType.empty;
-                                break;
-
-                            default:
-                                throw new Exception("Lex Error");
-                        }
+                    _index++;
+                    peek = _regexp[_index];
+                }
+                switch (peek)
+                {
+                    case (char)TokenType.lParen:
+                    case (char)TokenType.rParen:
+                    case (char)TokenType.plus:
+                    case (char)TokenType.minus:
+                    case (char)TokenType.mult:
+                    case (char)TokenType.div:
+                    case (char)TokenType.eof:
+                        tokenFound = true;
+                        result.tag = (TokenType)peek;
                         break;
                     default:
+                        tokenFound = true;
+                        result.tag = TokenType.number;
+                        result.value = peek;
                         break;
                 }
                 _index++;
             }
-            _state = 0;
             return result;
 
         }
